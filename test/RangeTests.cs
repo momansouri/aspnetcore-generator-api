aa0f683eafcd38ff4fc479c03506d4da84fa9302
mmansouri <mohamed.mansouri@sky.uk>
2017-10-20 16:38:47 +0100

    Added test

--- test/RangeTests.cs
+++ test/RangeTests.cs
@@ -14,5 +14,13 @@ namespace test
             var generated = range.Of(() => "");
             Assert.Equal(3, generated.Count());
         }
+
+        [Fact]
+        public void CountShouldControlNumberOfResults()
+        {
+            var range = new Range{Count = 3};
+            var generated = range.Of(() => "");
+            Assert.Equal(2, generated.Count());
+        }
     }
 }
